394f2ceffee14cb8080542f096e1ab8f1be6524c
csulwk <lwktechnology@qq.com>
2018-05-08 11:30:38 +0800

    创建Maven项目

---
+++ src/main/java/com/lwk/thread/ThreadTest.java
@@ -0,0 +1,13 @@
+package com.lwk.thread;
+
+/**
+ * @author lwk
+ * @date 2018/5/8 11:25
+ */
+public class ThreadTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("----END----");
+    }
+}
